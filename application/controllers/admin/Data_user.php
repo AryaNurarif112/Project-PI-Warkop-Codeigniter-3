8a70f1a231896dbe9c434c089391b95532d03818
AryaNurarif112 <aryanurarrif@gmail.com>
2022-06-10 10:42:28 +0700

    Membuat dan menambahkan fitur Data_user

---
+++ application/controllers/admin/Data_user.php
@@ -0,0 +1,28 @@
+<?php
+
+class Data_user extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        if ($this->session->userdata('role_id') != '1') {
+            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            Anda Belum Login!
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+          </div>');
+            redirect('auth/login');
+        }
+    }
+
+    public function index()
+    {
+        $this->load->model('Model_barang');
+        $data['user'] = $this->Model_barang->tampil_user()->result_array();
+        $this->load->view('templates_admin/header');
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/user', $data);
+        $this->load->view('templates_admin/footer');
+    }
+}
