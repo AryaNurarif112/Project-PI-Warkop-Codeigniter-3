bd370cf9dc360194c7939258c3d3f4a6396cb925
AryaNurarif112 <aryanurarrif@gmail.com>
2022-06-10 10:41:34 +0700

    Menambahkan fitur dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -18,17 +18,89 @@
             <span class="sr-only">Next</span>
         </button>
     </div>
-    <div class="row text-center mt-4">
-        <?php foreach ($barang as $brg) : ?>
-            <div class="card ml-3 mb-3" style="width: 16rem;">
-                <img class="fotoproduk" src="<?= base_url('uploads/') . $brg['gambar']; ?>" class="card-img-top" alt="">
-                <div class="card-body">
-                    <h5 class="card-title mb-1"><?= $brg['nama_brg']; ?></h5>
-                    <small><?= $brg['keterangan']; ?></small>
-                    <span class="badge badge-success d-block py-2 mb-3 mt-2">Rp. <?php echo number_format($brg['harga'], 0, ',', '.') ?></span>
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
 
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Dashboard Admin</h1>
+        </div>
+
+        <!-- Content Row -->
+        <div class="row">
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    User</div>
+                                <a href="<?php echo base_url('admin/Data_user') ?>">
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Daftar User</div>
+                                    <? echo $this->db->count_all('tb_user'); ?>
+                                </a>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-user fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        <?php endforeach; ?>
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Pembeli</div>
+                                <a href="<?php echo base_url('admin/invoice') ?>">
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">User Pembeli</div>
+                                    <? echo $this->db->count_all('tb_invoice'); ?>
+                                </a>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Barang
+                                </div>
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col-auto">
+                                        <a href="<?php echo base_url('admin/Data_barang') ?>">
+                                            <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">Total Barang Tersedia : </div>
+                                            <? echo $this->db->count_all('tb_barang'); ?>
+                                        </a>
+                                    </div>
+                                    <div class="col">
+                                        <div class="progress progress-sm mr-2">
+                                            <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Content Row -->
     </div>
-</div>
