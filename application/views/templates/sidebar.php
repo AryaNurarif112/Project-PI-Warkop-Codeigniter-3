557857bf1e380cd0105b37de63986fbe679fb5b7
AryaNurarif112 <aryanurarrif@gmail.com>
2022-06-11 14:26:11 +0700

    Merubah Sidebar 'Username' To 'Nama'

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -154,7 +154,7 @@
                         </div>
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-800 large">Hi <?php echo $this->session->userdata('username') ?></span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-800 large">Hi <?= $user['nama'] ?></span>
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
