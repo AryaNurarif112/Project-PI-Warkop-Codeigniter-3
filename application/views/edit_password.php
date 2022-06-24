869f832471a6cb93b03bbe26122eb13678172e33
AryaNurarif112 <aryanurarrif@gmail.com>
2022-06-24 13:12:00 +0700

    Change write in English to Indonesia

--- application/views/edit_password.php
+++ application/views/edit_password.php
@@ -8,23 +8,23 @@
             <?= $this->session->flashdata('message'); ?>
             <form action="<?= base_url('dashboard/changePassword') ?>" method="post">
                 <div class="form-group">
-                    <label for="current_password">Current Password</label>
+                    <label for="current_password">Password Lama</label>
                     <input type="password" class="form-control" id="current_password" name="current_password">
                     <?= form_error('current_password', ' <small class="text-danger pl-3">', '</smal>'); ?>
                 </div>
                 <div class="form-group">
-                    <label for="new_password1">New Password</label>
+                    <label for="new_password1">Password Terbaru</label>
                     <input type="password" class="form-control" id="new_password1" name="new_password1">
                     <?= form_error('new_password1', ' <small class="text-danger pl-3">', '</smal>'); ?>
 
                 </div>
                 <div class="form-group">
-                    <label for="new_password2">Repeat Password</label>
+                    <label for="new_password2">Ulangi Password Terbaru</label>
                     <input type="password" class="form-control" id="new_password2" name="new_password2">
                     <?= form_error('new_password2', ' <small class="text-danger pl-3">', '</smal>'); ?>
                 </div>
                 <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Change Password</button>
+                    <button type="submit" class="btn btn-primary">Ganti Password</button>
                 </div>
             </form>
             <?= form_close() ?>
