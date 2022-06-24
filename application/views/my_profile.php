343f5734c2ab5ad6b4acb311abbfb6b2040ac73c
AryaNurarif112 <aryanurarrif@gmail.com>
2022-06-24 13:12:41 +0700

    Delete Slider

--- application/views/my_profile.php
+++ application/views/my_profile.php
@@ -1,33 +1,12 @@
-<div class="container-fluid">
-
-    <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active" data-interval="10000">
-                <img src="<?php echo base_url('assets/img/slider1.jpg'); ?>" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item" data-interval="2000">
-                <img src="<?php echo base_url('assets/img/slider2.jpg'); ?>" class="d-block w-100" alt="...">
-            </div>
-        </div>
-        <button class="carousel-control-prev border-0" type="button" data-target="#carouselExampleInterval" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </button>
-        <button class="carousel-control-next border-0" type="button" data-target="#carouselExampleInterval" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </button>
-    </div>
-</div>
 <div class="table-responsive">
-    <h4>My Profile</h4>
+    <h4>Profile Saya</h4>
     <table class="table table-bordered table-hover table-striped">
         <tr>
             <td>Id User</td>
             <td>Nama</td>
             <td>Username</td>
             <td>Role Id</td>
-            <td>Member Since : </td>
+            <td>Pembuatan Member: </td>
         </tr>
         <tr>
             <td><?php echo $user['id'] ?></td>
@@ -38,7 +17,7 @@
         </tr>
 </div>
 </table>
-<div align="center">
+<div align="right">
     <a href="<?php echo base_url('welcome') ?>">
         <div class="btn btn-sm btn-dark">Lanjutkan Belanja</div>
         <a href="<?php echo base_url('dashboard/edit_profile') ?>">
