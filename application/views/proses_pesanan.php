ffb5892ea63af6a37d99cd8d015864a542f9f83b
AryaNurarif112 <aryanurarrif@gmail.com>
2022-08-10 15:46:16 +0700

    Change text

--- application/views/proses_pesanan.php
+++ application/views/proses_pesanan.php
@@ -1,13 +1,13 @@
 <div class="container-fluid">
     <div class="alert alert-success">
         <p class="text-center align-middle">
-            Silahkan bayar dan upload bukti pembayaran sesuai pesanan
+            Silahkan bayar dan upload bukti pembayaran sesuai pesanan melalui WhatsApp chat ini, ya!
         </p>
     </div>
     <div class="navbar">
         <ul class="nav navbar-nav navbar-right">
             <li class="mr-3 mb-2">
-                <a href="https://wa.me/6282135888258?text=Silahkan bayar dan upload bukti pembayaran sesuai pesanan ">
+                <a href="https://wa.me/6282135888258?text=Silahkan bayar dan upload bukti pembayaran sesuai pesanan melalui WhatsApp chat ini, ya! ">
                     <div class="">Upload Bukti Pembayaran</div>
                 </a>
             </li>
